37ad2bc5c1df8accf10f6b458cb53a5e85ff85fd
Gabriele Oliaro <oliaro.gabriele@vashonsd.org>
2016-02-02 09:01:41 -0800

    This is the new structure

--- Location.java
+++ Location.java
@@ -1,23 +1,74 @@
+import java.util.*;
 public class Location
 {
-   public static final int WATER = 0;
-   public static final int ISLAND = 1;
-   public static final int SHIP = 3;
+   private String name;
+   private HashMap<Location, Integer> relatedNodes;
+   private int info;
    
-   private int type;
-   
-   public Location()
+   public Location(String name)
    {
-      this.type = WATER;
+	   this.name = name;
+	   relatedNodes = new HashMap<Location, Integer>();
    }
    
-   public int getType()
+   public Location(String name, int info)
    {
-      return this.type;
+	   this.name = name;
+	   this.info = info;
+	   relatedNodes = new HashMap<Location, Integer>();
    }
    
-   public void setType(int type)
+   public String getName()
    {
-      this.type = type;
+	   return this.name;
+   }
+   
+   public void setName(String name)
+   {
+	   this.name = name;
+   }
+   
+   public HashMap<Location, Integer> getRelatedNodes()
+   {
+	   return this.relatedNodes;
+   }
+   
+   public int getInfo()
+   {
+	   return this.info;
+   }
+   
+   public void addEdge(Location next)
+   {
+	   relatedNodes.put(next, 0);
+   }
+   
+   public void addEdge(Location next, int distance)
+   {
+	   relatedNodes.put(next, distance);
+   }
+   
+   public void removeEdge(Location next)
+   {
+	   relatedNodes.remove(next);
+   }
+   
+   public boolean equals(Location other)
+   {
+	   return this.name == other.name && this.info == other.info && this.relatedNodes == other.relatedNodes;
+   }
+   
+   public String toString()
+   {
+	   String toReturn = "";
+	   toReturn += "This is node \"" + name + "\".\n";
+	   toReturn += "The information stored is: " + info + ".\n";
+	   toReturn += "The adjacent nodes are:\n";
+	   for(Location current:relatedNodes.keySet())
+	   {
+		   toReturn += "- Node \""+ current.getName() + "\" at a distance: " + relatedNodes.get(current) + "\n";
+	   }
+	   
+	   return toReturn;
    }
 }
