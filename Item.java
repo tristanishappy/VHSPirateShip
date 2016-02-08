0d19290079d62e66db0577fe159ccb1b3afd6a32
petersenpreston <petersen.preston@vashonsd.org>
2016-02-08 09:24:00 -0800

    ItemComments
    Added Comments to Item Class

--- Item.java
+++ Item.java
@@ -1,10 +1,18 @@
 
 public class Item {
+	//Initializing the item variables
 	private String name;
 	private String description;
 	private int quantity;
 	private int weight;
 
+	/**
+	 * Constructor for an Item.
+	 * @param name
+	 * @param description
+	 * @param quantity
+	 * @param weight
+	 */
 	public Item(String name, String description, int quantity, int weight) {
 		this.name = name; 
 		this.quantity = quantity;
@@ -12,34 +20,42 @@ public class Item {
 		this.weight = weight;
 	}
 	
+	//Method returning the Quantity of the item.
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
+	//Method returning the Description of the item.
 	public String getDescription() {
 		return description;
 	}
-	
+
+	//Method returning the Name of the item.
 	public String getName() {
 		return name;
 	}
-	
+
+	//Method returning the Weight of the item.
 	public int getWeight() {
 		return weight;
 	}
 	
+	//Method subtracting the amount of the item used from the quantity.
 	public void useItem(int x) {
 		quantity = quantity - x;
 	}
-	
+
+	//Method adding x amount to the quantity
 	public void addItem(int x) {
 		quantity = quantity + x;
 	}
 	
+	//Method setting the quantity to the value of x. 
 	public void setItemQuantity(int x) {
 		quantity = x;
 	}
 	
+	//Method returning the total weight of an item type.
 	public int totalWeight() {
 		return weight*quantity;
 	}
